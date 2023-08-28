ca69c6555ad248f2ffce0acc5107cd551ef66fd4
daishan <616479200@qq.com>
2023-08-28 23:53:01 +0800

    添加spdlog,给出一个简单的logger

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
+#include<config.h>
+#include"logger.h"
 
 namespace engine{
 void PrintEngine(){
+
+#ifdef _ENGINE_
+	EG_ERROR("Engine Starts! value = {}", 9);
+#endif
 	std::cout<< "Engine"<<std::endl;
 }
 }
